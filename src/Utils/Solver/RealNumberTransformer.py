4429b8e0db6b5f663db2c10f3e0e47c64653d855
Frank Ma <guang.y.ma@gmail.com>
2015-09-14 21:00:41 -0400

    add uni-bound real number transformer

--- src/Utils/Solver/RealNumberTransformer.py
+++ src/Utils/Solver/RealNumberTransformer.py
@@ -1,4 +1,4 @@
-from math import tan, atan, pi
+from math import tan, atan, pi, exp, log
 from scipy.stats import norm
 
 
@@ -7,34 +7,57 @@ __author__ = 'frank.ma'
 
 class RealNumberTransformer(object):
 
-    def __init__(self, l: float, u: float, method='abs'):
+    def __init__(self, l: float or None, u: float or None, method='abs'):
         self.l = l
         self.u = u
-        self.rgn = u - l
+        if l is not None and u is not None:
+            self.rgn = u - l
         self.method = method
 
     def uc_to_c(self, unconstraint: float):
 
-        if self.method == 'abs':
-            return self.l + 0.5 * self.rgn * (1.0 + unconstraint / (1 + abs(unconstraint)))
-        elif self.method == 'tan':
-            return self.l + self.rgn * (atan(unconstraint) / pi + 0.5)
-        elif self.method == 'norm':
-            return self.l + self.rgn * norm.cdf(unconstraint)
+        constraint = unconstraint
+
+        if self.l is None and self.u is None:
+            print('WARNING: neither lower nor upper bound is provided, no transform is performed.')
+        elif self.l is None:
+            constraint = self.u - exp(unconstraint)
+        elif self.u is None:
+            constraint = self.l + exp(unconstraint)
         else:
-            raise Exception("unrecognized transformation method")
+            if self.method == 'abs':
+                constraint = self.l + 0.5 * self.rgn * (1.0 + unconstraint / (1 + abs(unconstraint)))
+            elif self.method == 'tan':
+                constraint = self.l + self.rgn * (atan(unconstraint) / pi + 0.5)
+            elif self.method == 'norm':
+                constraint = self.l + self.rgn * norm.cdf(unconstraint)
+            else:
+                raise Exception('unrecognized transformation method')
+
+        return constraint
 
     def c_to_uc(self, constraint: float):
 
-        if self.method == 'abs':
-            mid = 0.5 * (self.l + self.u)
-            if constraint < mid:
-                return 1.0 - 0.5 * self.rgn / (constraint - self.l)
-            else:
-                return -1.0 - 0.5 * self.rgn / (constraint - self.u)
-        elif self.method == 'tan':
-            return tan(((constraint - self.l) / self.rgn - 0.5) * pi)
-        elif self.method == 'norm':
-            return norm.ppf(constraint - self.l) / self.rgn
+        unconstraint = constraint
+
+        if self.l is None and self.u is None:
+            print('WARNING: neither lower nor upper bound is provided, no transform is performed.')
+        elif self.l is None:
+            unconstraint = log(self.u - constraint)
+        elif self.u is None:
+            unconstraint = log(constraint - self.l)
         else:
-            raise Exception("unrecognized transformation method")
+            if self.method == 'abs':
+                mid = 0.5 * (self.l + self.u)
+                if constraint < mid:
+                    unconstraint = 1.0 - 0.5 * self.rgn / (constraint - self.l)
+                else:
+                    unconstraint = -1.0 - 0.5 * self.rgn / (constraint - self.u)
+            elif self.method == 'tan':
+                unconstraint = tan(((constraint - self.l) / self.rgn - 0.5) * pi)
+            elif self.method == 'norm':
+                unconstraint = norm.ppf(constraint - self.l) / self.rgn
+            else:
+                raise Exception("unrecognized transformation method")
+
+        return unconstraint
--- test/Utils/Solver/test_realNumberTransformer.py
+++ test/Utils/Solver/test_realNumberTransformer.py
@@ -12,9 +12,14 @@ class TestRealNumberTransformer(TestCase):
         lower_bound = 0.0
         upper_bound = 4.0
 
+        rnt_none = RealNumberTransformer(None, None)
+        self.assertEquals(1.0, rnt_none.uc_to_c(1.0))
+
         rnt_abs = RealNumberTransformer(lower_bound, upper_bound, 'abs')
         rnt_tan = RealNumberTransformer(lower_bound, upper_bound, 'tan')
         rnt_norm = RealNumberTransformer(lower_bound, upper_bound, 'norm')
+        rnt_lb = RealNumberTransformer(lower_bound, None)
+        rnt_ub = RealNumberTransformer(None, upper_bound)
 
         y_abs = x.copy()
         x_abs = x.copy()
@@ -22,6 +27,10 @@ class TestRealNumberTransformer(TestCase):
         x_tan = x.copy()
         y_norm = x.copy()
         x_norm = x.copy()
+        y_lb = x.copy()
+        x_lb = x.copy()
+        y_ub = x.copy()
+        x_ub = x.copy()
 
         for idx, v in enumerate(x):
             y_abs[idx] = rnt_abs.uc_to_c(v)
@@ -30,18 +39,29 @@ class TestRealNumberTransformer(TestCase):
             x_tan[idx] = rnt_tan.c_to_uc(y_tan[idx])
             y_norm[idx] = rnt_norm.uc_to_c(v)
             x_norm[idx] = rnt_norm.c_to_uc(x_norm[idx])
+            y_lb[idx] = rnt_lb.uc_to_c(v)
+            x_lb[idx] = rnt_lb.c_to_uc(y_lb[idx])
+            y_ub[idx] = rnt_ub.uc_to_c(v)
+            x_ub[idx] = rnt_ub.c_to_uc(y_ub[idx])
             self.assertAlmostEqual(v, x_abs[idx], places=12, msg='abs method check failed.')
             self.assertAlmostEqual(v, x_tan[idx], places=12, msg='tan method check failed.')
             # self.assertAlmostEqual(v, x_norm[idx], places=12, msg='norma method check failed.')
+            self.assertAlmostEqual(v, x_lb[idx], places=6, msg='lower bound log method check failed.')
+            self.assertAlmostEqual(v, x_ub[idx], places=6, msg='upper bound log method check failed.')
 
     def test_c_to_uc(self):
         y = np.array(range(0, 4)) * 0.01
         lower_bound = -1e-4
         upper_bound = 4.0 + 1e-4
 
+        rnt_none = RealNumberTransformer(None, None)
+        self.assertEquals(1.0, rnt_none.c_to_uc(1.0))
+
         rnt_abs = RealNumberTransformer(lower_bound, upper_bound, 'abs')
         rnt_tan = RealNumberTransformer(lower_bound, upper_bound, 'tan')
         rnt_norm = RealNumberTransformer(lower_bound, upper_bound, 'norm')
+        rnt_lb = RealNumberTransformer(lower_bound, None)
+        rnt_ub = RealNumberTransformer(None, upper_bound)
 
         y_abs = y.copy()
         x_abs = y.copy()
@@ -49,6 +69,10 @@ class TestRealNumberTransformer(TestCase):
         x_tan = y.copy()
         y_norm = y.copy()
         x_norm = y.copy()
+        y_lb = y.copy()
+        x_lb = y.copy()
+        y_ub = y.copy()
+        x_ub = y.copy()
 
         for idx, v in enumerate(y):
             x_abs[idx] = rnt_abs.c_to_uc(v)
@@ -57,6 +81,12 @@ class TestRealNumberTransformer(TestCase):
             y_tan[idx] = rnt_tan.uc_to_c(x_tan[idx])
             x_norm[idx] = rnt_norm.c_to_uc(v)
             y_norm[idx] = rnt_norm.uc_to_c(x_norm[idx])
+            x_lb[idx] = rnt_lb.c_to_uc(v)
+            y_lb[idx] = rnt_lb.uc_to_c(x_lb[idx])
+            x_ub[idx] = rnt_ub.c_to_uc(v)
+            y_ub[idx] = rnt_ub.uc_to_c(x_ub[idx])
             self.assertAlmostEqual(v, y_abs[idx], places=12, msg='abs method check failed.')
             self.assertAlmostEqual(v, y_tan[idx], places=12, msg='tan method check failed.')
             # self.assertAlmostEqual(v, y_norm[idx], places=12, msg='norm method check failed.')
+            self.assertAlmostEqual(v, y_lb[idx], places=6, msg='norm method check failed.')
+            self.assertAlmostEqual(v, y_ub[idx], places=6, msg='norm method check failed.')
