55c1a64871b80c595a02c85649613b95f0d7e7cb
Frank Ma <guang.y.ma@gmail.com>
2015-10-04 18:01:24 -0400

    minor changes to sabr forward distribution mini project

--- src/MiniProjects/SABRSimulation/SABRSimulator.py
+++ src/MiniProjects/SABRSimulation/SABRSimulator.py
@@ -16,16 +16,20 @@ class SABRSimulator(object):
         if not 1.0 >= self.rho >= -1.0:
             raise ValueError('correlation %r must in range [-1.0, 1.0]' % self.rho)
 
-    def calc_analytic_pdf(self, strikes: np.array) -> np.array:
+    def calc_sigmas(self, strikes: np.array) -> np.array:
         model = SABRModel(self.tau, self.alpha, self.beta, self.nu, self.rho)
-        vols = model.calc_lognormal_vol_vec_k(self.forward_0, strikes)
+        return model.calc_lognormal_vol_vec_k(self.forward_0, strikes)
+
+    def calc_analytic_pdf(self, strikes: np.array) -> np.array:
+        vols = self.calc_sigmas(strikes)
         return self.calc_analytic_pdf_given_vols(strikes, vols)
 
     def calc_analytic_pdf_given_vols(self, strikes: np.array, vols: np.array) -> np.array:
         means = np.log(self.forward_0)  # we know sabr is driftless as it models forward
         sigmas = vols * np.sqrt(self.tau)  # give volatility time scale
         density = 1.0 / sigmas / np.sqrt(2.0 * np.pi) * np.exp(-0.5 * ((np.log(strikes) - means) / sigmas)**2)
-        return density / self.forward_0
+        density /= self.forward_0  # need to be rescaled by forward
+        return density
 
     def calc_mc_pdf(self, strikes: np.array, n_steps: int = 100, n_scenarios: int = 10**5):
         forwards = self.simulate(n_steps, n_scenarios)
@@ -33,19 +37,24 @@ class SABRSimulator(object):
         bins_mid = 0.5 * (bins[:-1] + bins[1:])
         return bins_mid, freq
 
-    def simulate(self, n_steps: int, n_scenarios: int) -> np.array:
+    def simulate(self, n_steps: int, n_scenarios: int, is_log_space: bool = False) -> np.array:
         taus = np.linspace(self.tau, 0.0, num=n_steps)
         # initial setting up forward and sigma
         forwards = np.full(n_scenarios, self.forward_0)
         sigmas = np.full(n_scenarios, self.alpha)
 
         for idx, tau in enumerate(taus[1:]):
-            sqrt_dt = np.sqrt(taus[idx] - tau)
+            dt = taus[idx] - tau
+            sqrt_dt = np.sqrt(dt)
             # standard random variables with mean of zero, variance of one and correlation as given
             means = [0.0, 0.0]
             covariances = [[1.0, self.rho], [self.rho, 1.0]]
             rands = np.random.multivariate_normal(means, covariances, n_scenarios)
-            forwards += sigmas * forwards**self.beta * rands[:, 0] * sqrt_dt
-            sigmas += self.nu * sigmas * rands[:, 1] * sqrt_dt
+            if is_log_space:
+                forwards *= np.exp(-0.5 * sigmas**2 * dt + sigmas * rands[:, 0] * sqrt_dt)
+                sigmas *= np.exp(-0.5 * self.nu**2 * dt + self.nu * rands[:, 1] * sqrt_dt)
+            else:
+                forwards += sigmas * forwards**self.beta * rands[:, 0] * sqrt_dt
+                sigmas += self.nu * sigmas * rands[:, 1] * sqrt_dt
 
         return forwards
--- src/MiniProjects/SABRSimulation/main.py
+++ src/MiniProjects/SABRSimulation/main.py
@@ -6,22 +6,30 @@ from src.MiniProjects.SABRSimulation.SABRSimulator import SABRSimulator
 __author__ = 'frank.ma'
 
 
-fwd = 100.0
-tau = 0.25
-alpha = 0.2
+fwd = 150.0
+tau = 0.5
+alpha = 0.4
 beta = 1.0
-nu = 0.7
-rho = 0.25
+nu = 0.9
+rho = -0.4
 
-n_scn = 10**5
-n_stp = 5 * 10**2
+n_scn = 10**6
+n_stp = 10**2
 
+strikes = np.linspace(0.2 * fwd, 2.0 * fwd, num=101)
 sim = SABRSimulator(fwd, tau, alpha, beta, nu, rho)
-strikes = np.linspace(0.1 * fwd, 2.0 * fwd, num=101)
+
+sigmas = sim.calc_sigmas(strikes)
 analytic_density = sim.calc_analytic_pdf(strikes)
-flat_density = sim.calc_analytic_pdf_given_vols(strikes, alpha)
+flat_density = sim.calc_analytic_pdf_given_vols(strikes, sim.calc_sigmas(np.array([fwd])))
 forwards = sim.simulate(n_stp, n_scn)
-plt.hist(forwards, bins=strikes, normed=True)
+
+plt.subplot(2, 1, 1)
+plt.plot(strikes, sigmas, 'bx-')
+plt.xlim([strikes[0], strikes[-1]])
+plt.subplot(2, 1, 2)
+plt.hist(forwards, bins=strikes, normed=True, color='m')
 plt.plot(strikes, analytic_density, 'bx-')
 plt.plot(strikes, flat_density, 'r+-')
+plt.xlim([strikes[0], strikes[-1]])
 plt.show()
