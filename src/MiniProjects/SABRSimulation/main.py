b87c6b9b22271b07ab61c478a6c92fe7eeed1816
Frank Ma <guang.y.ma@gmail.com>
2015-10-06 21:32:15 -0400

    minor addition to sabr model simulation mini proj

--- src/MiniProjects/SABRSimulation/main.py
+++ src/MiniProjects/SABRSimulation/main.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 
 from math import sqrt
 from src.MiniProjects.SABRSimulation.SABRSimulator import SABRSimulator
+from src.SABRModel.SABRModel import SABRModel
+from src.Utils.Black76 import Black76
+from src.Utils.OptionType import OptionType
 
 __author__ = 'frank.ma'
 
@@ -17,38 +20,54 @@ rho = 0.0
 n_scn = 10**6
 n_stp = 10**2
 
-strikes = np.linspace(0.2 * fwd, 2.0 * fwd, num=101)
+strikes = np.linspace(0.2 * fwd, 4.0 * fwd, num=201)
 strikes_mid = 0.5 * (strikes[:-1] + strikes[1:])
-strikes_ana = np.linspace(0.1 * fwd, 10.0 * fwd, num=1001)
-strikes_ana_mid = 0.5 * (strikes_ana[:-1] + strikes_ana[1:])
-strikes_ana_inc = np.subtract(strikes_ana[1:], strikes_ana[:-1])
+strikes_inc = np.subtract(strikes[1:], strikes[:-1])
 
 sim = SABRSimulator(fwd, tau, alpha, beta, nu, rho)
 
-sigma_atm = sim.calc_sigmas(np.array([fwd]))
-sigmas = sim.calc_sigmas(strikes_ana_mid)
-var_den = sim.calc_analytic_pdf(strikes_ana_mid)
-flat_den = sim.calc_pdf_given_vols(strikes_ana_mid, sigma_atm)
-forwards = sim.simulate(n_stp, n_scn)
+sim_fwds_terminal = sim.simulate(n_stp, n_scn)
 
-sim_mean = np.average(forwards)
-sim_std = np.std(forwards)
-var_mean = sum(strikes_ana_mid * var_den * strikes_ana_inc)
-var_std = sqrt(sum(np.subtract(strikes_ana_mid, var_mean)**2 * var_den * strikes_ana_inc))
-flat_mean = sum(strikes_ana_mid * flat_den * strikes_ana_inc)
-flat_std = sqrt(sum(np.subtract(strikes_ana_mid, flat_mean)**2 * flat_den * strikes_ana_inc))
+var_vols = sim.calc_sigmas(strikes_mid)
+var_den = sim.calc_analytic_pdf(strikes_mid)
+flat_vols = sim.calc_sigmas(np.array([fwd]))
+flat_den = sim.calc_pdf_given_vols(strikes_mid, flat_vols)
+
+sim_mean = np.average(sim_fwds_terminal)
+sim_std = np.std(sim_fwds_terminal)
+var_mean = sum(strikes_mid * var_den * strikes_inc)
+var_std = sqrt(sum(np.subtract(strikes_mid, var_mean)**2 * var_den * strikes_inc))
+flat_mean = sum(strikes_mid * flat_den * strikes_inc)
+flat_std = sqrt(sum(np.subtract(strikes_mid, flat_mean)**2 * flat_den * strikes_inc))
 
 print('\tmean\tstd')
 print('sim\t%.6f\t%6f' % (sim_mean, sim_std))
-print('var vol\t%6f\t%6f\t%6f' % (var_mean, var_std, sum(var_den * strikes_ana_inc)))
-print('flat vol\t%6f\t%6f\t%6f' % (flat_mean, flat_std, sum(flat_den * strikes_ana_inc)))
+print('var vol\t%6f\t%6f\t%6f' % (var_mean, var_std, sum(var_den * strikes_inc)))
+print('flat vol\t%6f\t%6f\t%6f' % (flat_mean, flat_std, sum(flat_den * strikes_inc)))
+
+print('option pricing\nsimulated\tanalytic')
+den_y, bins = np.histogram(sim_fwds_terminal, bins=strikes, normed=True)
+den_x = (bins[1:] + bins[:-1]) * 0.5
+
+k = 155.0
+b = 0.95
+opt_type = OptionType.call
+
+payoff = np.maximum(opt_type.value * np.subtract(den_x, k), np.zeros(den_y.__len__()))
+den_inc = np.subtract(bins[1:], bins[:-1])
+simulated_price = b * sum(payoff * den_y * den_inc)
+
+black_vol = SABRModel(tau, alpha, beta, nu, rho).calc_lognormal_vol(fwd, k)
+black_price = Black76.price(fwd, k, tau, black_vol, b, opt_type)
+
+print('%.6f\t%.6f' % (simulated_price, black_price))
 
 plt.subplot(2, 1, 1)
-plt.plot(strikes_ana_mid, sigmas, 'b')
+plt.plot(strikes_mid, var_vols, 'b')
 plt.xlim([strikes[0], strikes[-1]])
 plt.subplot(2, 1, 2)
-plt.hist(forwards, bins=strikes, normed=True, color='w')
-plt.plot(strikes_ana_mid, var_den, 'b')
-plt.plot(strikes_ana_mid, flat_den, 'r')
+plt.hist(sim_fwds_terminal, bins=strikes, normed=True, color='w')
+plt.plot(strikes_mid, var_den, 'b')
+plt.plot(strikes_mid, flat_den, 'r')
 plt.xlim([strikes[0], strikes[-1]])
 plt.show()
