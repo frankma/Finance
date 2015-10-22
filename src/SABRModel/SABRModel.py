7ec93d00a656103406974939d232275eadd43790
frankma <guang.y.ma@gmail.com>
2015-10-22 12:23:13 -0400

    clean up SABR model

--- src/SABRModel/SABRModel.py
+++ src/SABRModel/SABRModel.py
@@ -1,6 +1,5 @@
-from math import log, sqrt
-
 import numpy as np
+
 from numpy.polynomial.polynomial import polyroots
 
 from src.Utils.VolType import VolType
@@ -40,6 +39,21 @@ class SABRModel(object):
         strikes_mid = 0.5 * (strikes[:-1] + strikes[1:])
         return freq, strikes_mid
 
+    def get_lognormal_approx(self):
+        return SABRModelLognormalApprox(self.t, self.alpha, self.beta, self.nu, self.rho)
+
+    def get_normal_approx(self):
+        return SABRModelNormalApprox(self.t, self.alpha, self.beta, self.nu, self.rho)
+
+    def calc_z(self, forward, strike):
+        return self.nu / self.alpha * np.log(forward / strike) / ((forward * strike) ** ((self.beta - 1.0) / 2.0))
+
+    def calc_z_norm(self, forward, strike):
+        return self.nu / self.alpha * (forward - strike)
+
+    def calc_x(self, z):
+        return np.log((np.sqrt(1.0 - 2.0 * self.rho * z + z ** 2) + z - self.rho) / (1.0 - self.rho))
+
     def calc_vol(self, forward: float, strike: float, vol_type: VolType) -> float:
         raise ValueError('unexpected call of abstract method')
 
@@ -63,23 +77,29 @@ class SABRModelLognormalApprox(SABRModel):
 
     def calc_vol(self, forward: float, strike: float, vol_type: VolType = VolType.black) -> float:
         one_m_beta = 1.0 - self.beta
+        f_min_k = forward - strike
         f_mul_k = forward * strike
-        f_per_k = forward / strike
+        ln_f_per_k = np.log(forward / strike)
 
         term1, term2, term3 = 1.0, 1.0, 1.0
-        if abs(strike - forward) < 1e-10:
-            # At-The-Money option reduces term1 and term2 as log(f / k_atm) is zero
-            term1 = self.alpha / forward ** one_m_beta
+        if abs(self.beta) < 1e-12:
+            if abs(f_min_k) < 1e-12:
+                term1 = self.alpha / forward
+            else:
+                term1 = self.alpha * ln_f_per_k / f_min_k
+            term3 = 1.0 + ((self.alpha ** 2) / 24.0 / f_mul_k +
+                           (2.0 - 3.0 * (self.rho ** 2)) * (self.nu ** 2) / 24.0) * self.t
         else:
-            z = self.nu / self.alpha * f_mul_k ** (one_m_beta / 2.0) * log(f_per_k)
-            x = log((sqrt(1.0 - 2.0 * self.rho * z + z ** 2) + z - self.rho) / (1.0 - self.rho))
-            term1 = self.alpha / (f_mul_k ** (one_m_beta / 2.0) *
-                                  (1.0 + one_m_beta ** 2 / 24.0 *
-                                   (log(f_per_k)) ** 2 + one_m_beta ** 4 / 1920.0 * (log(f_per_k)) ** 4))
+            term1 = self.alpha / (f_mul_k ** (one_m_beta / 2.0)) / \
+                    (1.0 + (one_m_beta ** 2) * (ln_f_per_k ** 2) / 24.0 +
+                     (one_m_beta ** 4) * (ln_f_per_k ** 4) / 1920.0)
+            term3 = (1.0 + (one_m_beta ** 2 / 24.0 * self.alpha ** 2 / (f_mul_k ** one_m_beta) +
+                            0.25 * self.rho * self.beta * self.nu * self.alpha / f_mul_k ** (one_m_beta / 2.0) +
+                            (2.0 - 3.0 * self.rho ** 2) / 24.0 * self.nu ** 2) * self.t)
+        if abs(f_min_k) > 1e-12:
+            z = self.calc_z(forward, strike)
+            x = self.calc_x(z)
             term2 = z / x
-        term3 = (1.0 + (one_m_beta ** 2 / 24.0 * self.alpha ** 2 / (f_mul_k ** one_m_beta) +
-                        0.25 * self.rho * self.beta * self.nu * self.alpha / f_mul_k ** (one_m_beta / 2.0) +
-                        (2.0 - 3.0 * self.rho ** 2) / 24.0 * self.nu ** 2) * self.t)
         black_vol = term1 * term2 * term3
 
         if vol_type == VolType.black:
@@ -91,21 +111,30 @@ class SABRModelLognormalApprox(SABRModel):
             raise ValueError('unrecognized volatility type %s' % vol_type.__str__())
 
     def calc_vol_vec_k(self, forward: float, strikes: np.array, vol_type: VolType = VolType.black) -> np.array:
-        is_not_atm = np.abs(strikes - forward) > 1e-6
         n = strikes.__len__()
-        one_m_beta = 1.0 - self.beta
+        f_min_k = forward - strikes
         f_mul_k = forward * strikes
-        f_per_k = forward / strikes
+        ln_f_per_k = np.log(forward / strikes)
+        is_not_atm = np.abs(f_min_k) > 1e-12
+        is_atm = np.logical_not(is_not_atm)
+        one_m_beta = 1.0 - self.beta
 
         term1, term2, term3 = np.ones(n), np.ones(n), np.ones(n)
-        z = self.nu / self.alpha * f_mul_k ** (one_m_beta / 2.0) * np.log(f_per_k)
-        x = np.log((np.sqrt(1.0 - 2.0 * self.rho * z + z ** 2) + z - self.rho) / (1.0 - self.rho))
-        term1 = self.alpha / (f_mul_k ** (one_m_beta / 2.0) * (1.0 + one_m_beta ** 2 / 24.0 * (np.log(f_per_k)) ** 2 +
-                                                               one_m_beta ** 4 / 1920.0 * (np.log(f_per_k)) ** 4))
+        if abs(self.beta) < 1e-12:
+            term1[is_not_atm] = self.alpha * ln_f_per_k[is_not_atm] / f_min_k[is_not_atm]
+            term1[is_atm] = self.alpha / forward  # only take special care if there are zero / zero case
+            term3 = 1.0 + ((self.alpha ** 2) / 24.0 / f_mul_k +
+                           (2.0 - 3.0 * (self.rho ** 2)) * (self.nu ** 2) / 24.0) * self.t
+        else:
+            term1 = self.alpha / (f_mul_k ** (one_m_beta / 2.0)) / \
+                    (1.0 + (one_m_beta ** 2) * (ln_f_per_k ** 2) / 24.0 +
+                     (one_m_beta ** 4) * (ln_f_per_k ** 4) / 1920.0)
+            term3 = (1.0 + (one_m_beta ** 2 / 24.0 * self.alpha ** 2 / (f_mul_k ** one_m_beta) +
+                            0.25 * self.rho * self.beta * self.nu * self.alpha / f_mul_k ** (one_m_beta / 2.0) +
+                            (2.0 - 3.0 * self.rho ** 2) / 24.0 * self.nu ** 2) * self.t)
+        z = self.calc_z(forward, strikes)
+        x = self.calc_x(z)
         term2[is_not_atm] = z[is_not_atm] / x[is_not_atm]
-        term3 = (1.0 + (one_m_beta ** 2 / 24.0 * self.alpha ** 2 / (f_mul_k ** one_m_beta) +
-                        0.25 * self.rho * self.beta * self.nu * self.alpha / f_mul_k ** (one_m_beta / 2.0) +
-                        (2.0 - 3.0 * self.rho ** 2) / 24.0 * self.nu ** 2) * self.t)
         black_vols = term1 * term2 * term3
 
         if vol_type == VolType.black:
@@ -117,22 +146,30 @@ class SABRModelLognormalApprox(SABRModel):
             raise ValueError('unrecognized volatility type %s' % vol_type.__str__())
 
     def calc_vol_vec_f(self, forwards: np.array, strike: float, vol_type: VolType = VolType.black) -> np.array:
-        is_not_atm = np.abs(strike - forwards) > 1e-6
-        one_m_beta = 1.0 - self.beta
+        n = forwards.__len__()
+        f_min_k = forwards - strike
         f_mul_k = forwards * strike
-        f_per_k = forwards / strike
+        ln_f_per_k = np.log(forwards / strike)
+        is_not_atm = np.abs(f_min_k) > 1e-12
+        is_atm = np.logical_not(is_not_atm)
+        one_m_beta = 1.0 - self.beta
 
-        term1, term2, term3 = np.ones(forwards.__len__()), np.ones(forwards.__len__()), np.ones(forwards.__len__())
-
-        z = self.nu / self.alpha * f_mul_k ** (one_m_beta / 2.0) * np.log(f_per_k)
-        x = np.log((np.sqrt(1.0 - 2.0 * self.rho * z + z ** 2) + z - self.rho) / (1.0 - self.rho))
-
-        term1 = self.alpha / (f_mul_k ** (one_m_beta / 2.0) * (1.0 + one_m_beta ** 2 / 24.0 * (np.log(f_per_k)) ** 2 +
-                                                               one_m_beta ** 4 / 1920.0 * (np.log(f_per_k)) ** 4))
+        term1, term2, term3 = np.ones(n), np.ones(n), np.ones(n)
+        if abs(self.beta) < 1e-12:
+            term1[is_not_atm] = self.alpha * ln_f_per_k[is_not_atm] / f_min_k[is_not_atm]
+            term1[is_atm] = self.alpha / forwards[is_atm]  # only take special care if there are zero / zero case
+            term3 = 1.0 + ((self.alpha ** 2) / 24.0 / f_mul_k +
+                           (2.0 - 3.0 * (self.rho ** 2)) * (self.nu ** 2) / 24.0) * self.t
+        else:
+            term1 = self.alpha / (f_mul_k ** (one_m_beta / 2.0)) / \
+                    (1.0 + (one_m_beta ** 2) * (ln_f_per_k ** 2) / 24.0 +
+                     (one_m_beta ** 4) * (ln_f_per_k ** 4) / 1920.0)
+            term3 = (1.0 + (one_m_beta ** 2 / 24.0 * self.alpha ** 2 / (f_mul_k ** one_m_beta) +
+                            0.25 * self.rho * self.beta * self.nu * self.alpha / f_mul_k ** (one_m_beta / 2.0) +
+                            (2.0 - 3.0 * self.rho ** 2) / 24.0 * self.nu ** 2) * self.t)
+        z = self.calc_z(forwards, strike)
+        x = self.calc_x(z)
         term2[is_not_atm] = z[is_not_atm] / x[is_not_atm]
-        term3 = (1.0 + (one_m_beta ** 2 / 24.0 * self.alpha ** 2 / (f_mul_k ** one_m_beta) +
-                        0.25 * self.rho * self.beta * self.nu * self.alpha / f_mul_k ** (one_m_beta / 2.0) +
-                        (2.0 - 3.0 * self.rho ** 2) / 24.0 * self.nu ** 2) * self.t)
         black_vols = term1 * term2 * term3
 
         if vol_type == VolType.black:
@@ -183,30 +220,25 @@ class SABRModelNormalApprox(SABRModel):
         one_m_beta = 1.0 - self.beta
         f_mul_k = forward * strike
 
-        term1, term2, term3 = self.alpha * (f_mul_k ** (self.beta / 2.0)), 1.0, 1.0
-        if abs(forward - strike) > 1e-10:
-            if abs(self.beta) < 1e-10:
-                z = self.nu / self.alpha * (forward - strike)
-                term3 = 1.0 + (2.0 - 3.0 * self.rho ** 2) / 24.0 * (self.nu ** 2) * self.t
-            else:
-                f_per_k = forward / strike
-                ln_f_per_k = log(f_per_k)
-                term1 *= (1.0 + (ln_f_per_k ** 2) / 24.0 + (ln_f_per_k ** 4) / 1920.0) / \
-                         (1.0 + (one_m_beta ** 2) / 24.0 * (ln_f_per_k ** 2) + (one_m_beta ** 4) / 1920.0 * (
-                             ln_f_per_k ** 4))
-                z = self.nu / self.alpha * f_mul_k ** (one_m_beta / 2.0) * log(f_per_k)
-                term3 = 1.0 + (-self.beta * (2.0 - self.beta) * self.alpha ** 2 / 24.0 / (f_mul_k ** one_m_beta) +
-                               self.rho * self.alpha * self.nu * self.beta / 4.0 / (f_mul_k ** (one_m_beta / 2.0)) +
-                               (2.0 - 3.0 * self.rho ** 2) / 24.0 * self.nu ** 2) * self.t
-            x = log((sqrt(1.0 - 2.0 * self.rho * z + z ** 2) + z - self.rho) / (1.0 - self.rho))
-            term2 = z / x
+        term1, term2, term3 = self.alpha, 1.0, 1.0
+        if abs(self.beta) < 1e-12:
+            if abs(forward - strike) > 1e-12:
+                z = self.calc_z_norm(forward, strike)
+                x = self.calc_x(z)
+                term2 = z / x
+            term3 = 1.0 + ((2.0 - 3.0 * self.rho ** 2) / 24.0 * self.nu ** 2) * self.t
         else:
-            if abs(self.beta) < 1e-10:
-                term3 = 1.0 + ((2.0 - 3.0 * self.rho ** 2) / 24.0 * self.nu ** 2) * self.t
-            else:
-                term3 = 1.0 + (-self.beta * (2.0 - self.beta) * self.alpha ** 2 / 24.0 / (f_mul_k ** one_m_beta) +
-                               self.rho * self.alpha * self.nu * self.beta / 4.0 / (f_mul_k ** (one_m_beta / 2.0)) +
-                               (2.0 - 3.0 * self.rho ** 2) / 24.0 * self.nu ** 2) * self.t
+            ln_f_per_k = np.log(forward / strike)
+            term1 *= (f_mul_k ** (self.beta / 2.0)) * (1.0 + (ln_f_per_k ** 2) / 24.0 + (ln_f_per_k ** 4) / 1920.0) / \
+                     (1.0 + (one_m_beta ** 2) * (ln_f_per_k ** 2) / 24.0 +
+                      (one_m_beta ** 4) * (ln_f_per_k ** 4) / 1920.0)
+            if abs(forward - strike) > 1e-12:
+                z = self.calc_z(forward, strike)
+                x = self.calc_x(z)
+                term2 = z / x
+            term3 = 1.0 + (-self.beta * (2.0 - self.beta) * (self.alpha ** 2) / 24.0 / (f_mul_k ** one_m_beta) +
+                           self.rho * self.alpha * self.nu * self.beta / 4.0 / (f_mul_k ** (one_m_beta / 2.0)) +
+                           (2.0 - 3.0 * (self.rho ** 2)) * (self.nu ** 2) / 24.0) * self.t
         normal_vol = term1 * term2 * term3
 
         if vol_type == VolType.black:
@@ -223,24 +255,23 @@ class SABRModelNormalApprox(SABRModel):
         one_m_beta = 1.0 - self.beta
         f_mul_k = forward * strikes
 
-        term1, term2, term3 = self.alpha * (f_mul_k ** (self.beta / 2.0)), np.ones(n), np.ones(n)
+        term1, term2, term3 = np.full(n, self.alpha), np.ones(n), np.ones(n)
         if abs(self.beta) < 1e-12:
-            z = self.nu / self.alpha * (forward - strikes)
-            x = np.log((np.sqrt(1.0 - 2.0 * self.rho * z + z ** 2) + z - self.rho) / (1.0 - self.rho))
+            z = self.calc_z_norm(forward, strikes)
+            x = self.calc_x(z)
             term2[is_not_atm_or_zero_k] = z[is_not_atm_or_zero_k] / x[is_not_atm_or_zero_k]
             term3 = 1.0 + ((2.0 - 3.0 * self.rho ** 2) / 24.0 * self.nu ** 2) * self.t
         else:
-            f_per_k = forward / strikes
-            ln_f_per_k = np.log(f_per_k)
-            term1 *= (1.0 + (ln_f_per_k ** 2) / 24.0 + (ln_f_per_k ** 4) / 1920.0) / \
-                     (1.0 + (one_m_beta ** 2) / 24.0 * (ln_f_per_k ** 2) + (one_m_beta ** 4) / 1920.0 * (
-                         ln_f_per_k ** 4))
-            z = self.nu / self.alpha * (f_mul_k ** (one_m_beta / 2.0)) * np.log(f_per_k)
-            x = np.log((np.sqrt(1.0 - 2.0 * self.rho * z + z ** 2) + z - self.rho) / (1.0 - self.rho))
+            ln_f_per_k = np.log(forward / strikes)
+            term1 *= (f_mul_k ** (self.beta / 2.0)) * (1.0 + (ln_f_per_k ** 2) / 24.0 + (ln_f_per_k ** 4) / 1920.0) / \
+                     (1.0 + (one_m_beta ** 2) * (ln_f_per_k ** 2) / 24.0 +
+                      (one_m_beta ** 4) * (ln_f_per_k ** 4) / 1920.0)
+            z = self.calc_z(forward, strikes)
+            x = self.calc_x(z)
             term2[is_not_atm_or_zero_k] = z[is_not_atm_or_zero_k] / x[is_not_atm_or_zero_k]
-            term3 = 1.0 + (-self.beta * (2.0 - self.beta) * self.alpha ** 2 / 24.0 / (f_mul_k ** one_m_beta) +
+            term3 = 1.0 + (-self.beta * (2.0 - self.beta) * (self.alpha ** 2) / 24.0 / (f_mul_k ** one_m_beta) +
                            self.rho * self.alpha * self.nu * self.beta / 4.0 / (f_mul_k ** (one_m_beta / 2.0)) +
-                           (2.0 - 3.0 * self.rho ** 2) / 24.0 * self.nu ** 2) * self.t
+                           (2.0 - 3.0 * (self.rho ** 2)) * (self.nu ** 2) / 24.0) * self.t
         normal_vols = term1 * term2 * term3
 
         if vol_type == VolType.black:
@@ -257,24 +288,23 @@ class SABRModelNormalApprox(SABRModel):
         one_m_beta = 1.0 - self.beta
         f_mul_k = forwards * strike
 
-        term1, term2, term3 = self.alpha * (f_mul_k ** (self.beta / 2.0)), np.ones(n), np.ones(n)
+        term1, term2, term3 = np.full(n, self.alpha), np.ones(n), np.ones(n)
         if abs(self.beta) < 1e-12:
-            z = self.nu / self.alpha * (forwards - strike)
-            x = np.log((np.sqrt(1.0 - 2.0 * self.rho * z + z ** 2) + z - self.rho) / (1.0 - self.rho))
+            z = self.calc_z_norm(forwards, strike)
+            x = self.calc_x(z)
             term2[is_not_atm_or_zero_f] = z[is_not_atm_or_zero_f] / x[is_not_atm_or_zero_f]
             term3 = 1.0 + ((2.0 - 3.0 * self.rho ** 2) / 24.0 * self.nu ** 2) * self.t
         else:
-            f_per_k = forwards / strike
-            ln_f_per_k = np.log(f_per_k)
-            term1 *= (1.0 + (ln_f_per_k ** 2) / 24.0 + (ln_f_per_k ** 4) / 1920.0) / \
-                     (1.0 + (one_m_beta ** 2) / 24.0 * (ln_f_per_k ** 2) + (one_m_beta ** 4) / 1920.0 * (
-                         ln_f_per_k ** 4))
-            z = self.nu / self.alpha * f_mul_k ** (one_m_beta / 2.0) * np.log(f_per_k)
-            x = np.log((np.sqrt(1.0 - 2.0 * self.rho * z + z ** 2) + z - self.rho) / (1.0 - self.rho))
+            ln_f_per_k = np.log(forwards / strike)
+            term1 *= (f_mul_k ** (self.beta / 2.0)) * (1.0 + (ln_f_per_k ** 2) / 24.0 + (ln_f_per_k ** 4) / 1920.0) / \
+                     (1.0 + (one_m_beta ** 2) * (ln_f_per_k ** 2) / 24.0 +
+                      (one_m_beta ** 4) * (ln_f_per_k ** 4) / 1920.0)
+            z = self.calc_z(forwards, strike)
+            x = self.calc_x(z)
             term2[is_not_atm_or_zero_f] = z[is_not_atm_or_zero_f] / x[is_not_atm_or_zero_f]
-            term3 = 1.0 + (-self.beta * (2.0 - self.beta) * self.alpha ** 2 / 24.0 / (f_mul_k ** one_m_beta) +
+            term3 = 1.0 + (-self.beta * (2.0 - self.beta) * (self.alpha ** 2) / 24.0 / (f_mul_k ** one_m_beta) +
                            self.rho * self.alpha * self.nu * self.beta / 4.0 / (f_mul_k ** (one_m_beta / 2.0)) +
-                           (2.0 - 3.0 * self.rho ** 2) / 24.0 * self.nu ** 2) * self.t
+                           (2.0 - 3.0 * (self.rho ** 2)) * (self.nu ** 2) / 24.0) * self.t
         normal_vols = term1 * term2 * term3
 
         if vol_type == VolType.black:
@@ -297,7 +327,6 @@ class SABRModelNormalApprox(SABRModel):
 
     @staticmethod
     def solve_alpha(forward: float, vol_atm: float, t: float, beta: float, nu: float, rho: float):
-        f_pwr_one_m_beta = forward ** (1.0 - beta)
         cubic = -beta * (2.0 - beta) * t / 24.0 / (forward ** (2.0 - 3.0 * beta))
         quadratic = beta * nu * rho * t / 4.0 / (forward ** (1.0 - 2.0 * beta))
         linear = (1.0 + (nu ** 2) * (2.0 - 3.0 * (rho ** 2)) * t / 24.0) * (forward ** beta)
