f60953ca9d285d3d741132d10bc65b9a06212bc3
Frank Ma <guang.y.ma@gmail.com>
2015-10-31 00:19:51 -0400

    minor improvement to BS PDE

--- src/MiniProjects/HeatEquation/BlackScholesFD.py
+++ src/MiniProjects/HeatEquation/BlackScholesFD.py
@@ -2,6 +2,7 @@ import numpy as np
 from src.FDSolver.TriDiagonalMatrix import TriDiagonalMatrix
 from src.Utils.BSM import BSM
 from src.Utils.OptionType import OptionType
+import matplotlib.pyplot as plt
 
 __author__ = 'frank.ma'
 
@@ -15,25 +16,33 @@ class BlackScholesFD(object):
         self.s = s
         self.r = r
         self.sig = sig
-        self.dx = 1.0 / n_x
-        self.dt = 1.0 / n_t
+        self.xs = np.linspace(0.0, domain * k, num=n_x)
         self.ts = np.linspace(0.0, t, num=n_t)
-        self.xs = np.linspace(1e-6, domain * k, num=n_x)
+        self.dx = self.xs[1] - self.xs[0]
+        self.dt = self.ts[1] - self.ts[0]
+        self.sdx = self.xs / self.dx
+        self.tdx = self.ts / self.dt
 
     def solve(self):
         state = np.matrix(np.maximum(self.xs - self.k, np.zeros(self.n_x, dtype=float))).transpose()
-        lhs_l = (((self.sig * self.xs) ** 2) - self.r * self.xs * self.dx) / 4.0
-        lhs_c = (self.dx ** 2) / self.dt - (((self.sig * self.xs) ** 2) + self.r * (self.dx ** 2)) / 2.0
-        lhs_u = (((self.sig * self.xs) ** 2) + self.r * self.xs * self.dx) / 4.0
-        lhs_tdm = TriDiagonalMatrix(lhs_l[1:], lhs_c, lhs_u[:-1])
-        rhs_l = -lhs_l
-        rhs_c = (self.dx ** 2) / self.dt + (((self.sig * self.xs) ** 2) + self.r * (self.dx ** 2)) / 2.0
-        rhs_u = -lhs_u
-        rhs_tdm = TriDiagonalMatrix(rhs_l[1:], rhs_c, rhs_u[:-1])
+        lhs_lft = -self.r * self.sdx / 4.0 + ((self.sig * self.sdx) ** 2) / 4.0
+        lhs_ctr = 1.0 / self.dt - ((self.sig * self.sdx) ** 2) / 2.0 - self.r / 2.0
+        lhs_upr = self.r * self.sdx / 4.0 + ((self.sig * self.sdx) ** 2) / 4.0
+        lhs_tdm = TriDiagonalMatrix(lhs_lft[1:], lhs_ctr, lhs_upr[:-1])
+        rhs_lft = -lhs_lft
+        rhs_ctr = 1.0 / self.dt + ((self.sig * self.sdx) ** 2) / 2.0 + self.r / 2.0
+        rhs_upr = -lhs_upr
+        rhs_tdm = TriDiagonalMatrix(rhs_lft[1:], rhs_ctr, rhs_upr[:-1])
 
-        for t in self.ts[:-1]:
-            print(state.transpose())
+        plt.plot(self.xs, state)
+        # show_idx = [1, int(0.2 * self.n_t), int(0.5 * self.n_t), int(0.75 * self.n_t), self.n_t - 1]
+        show_idx = [1]
+        for idx, t in enumerate(self.ts[:-1]):
             state = lhs_tdm.get_inverse() * rhs_tdm.get_matrix() * state
+            if idx in show_idx:
+                plt.plot(self.xs, state)
+        plt.legend(self.ts[np.array([0] + show_idx)])
+        plt.show()
 
         vec = np.asarray(state.transpose()).reshape(-1)
         # print(vec)
@@ -46,9 +55,9 @@ if __name__ == '__main__':
     spot = 150.0
     strike = 155.0
     rate = 0.01
-    vol = 0.25
+    vol = 0.5
 
-    bsm_fd = BlackScholesFD(tau, spot, strike, rate, vol, n_x=11, n_t=11)
+    bsm_fd = BlackScholesFD(tau, spot, strike, rate, vol, n_x=101, n_t=11)
     bsm_fd.solve()
 
     ana = BSM.price(spot, strike, tau, rate, 0.0, vol, OptionType.call)
