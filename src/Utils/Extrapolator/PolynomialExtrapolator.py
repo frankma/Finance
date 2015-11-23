f8bf51e3532e04bfa12cd2848121c742805c7f1a
Frank Ma <guang.y.ma@gmail.com>
2015-11-23 07:42:12 -0500

    add volsurface wing fit function to Polynomial Extrapolator

--- src/Utils/Extrapolator/PolynomialExtrapolator.py
+++ src/Utils/Extrapolator/PolynomialExtrapolator.py
@@ -1,6 +1,8 @@
 import numpy as np
 from numpy.polynomial.polynomial import polyval
 
+from src.Utils.OptionType import OptionType
+
 __author__ = 'frank.ma'
 
 
@@ -19,6 +21,24 @@ class PolynomialExtrapolator(object):
     def extrapolate(self, xs: np.array):
         return self.zeroth_order(xs, self.betas, self.lam, self.is_reciprocal)
 
+    # fit to the wing quote on the volatility surface, assume lower strikes are put quotes and higher strikes are calls
+    @staticmethod
+    def quadratic_fit_vs_wing(lam: float, strike: float, price: float, delta_k: float, gamma_k: float,
+                              opt_type: OptionType):
+        is_reciprocal = opt_type == OptionType.call
+        base = PolynomialExtrapolator.__get_base(strike, is_reciprocal)
+        first = delta_k
+        second = gamma_k
+        if is_reciprocal:
+            first /= -base ** 2
+            second -= first * 2.0 * (base ** 3)
+            second /= base ** 4
+        beta_2 = 0.5 * (second / price - ((first / price) ** 2) + lam / (base ** 2))
+        beta_1 = first / price - lam / base - 2.0 * beta_2 * base
+        beta_0 = np.log(price / (base ** lam)) - beta_1 * base - beta_2 * (base ** 2)
+        betas = np.array([beta_0, beta_1, beta_2])
+        return PolynomialExtrapolator(lam, betas, is_reciprocal)
+
     @staticmethod
     def __get_base(xs: np.array, is_reciprocal: bool):
         if is_reciprocal:
@@ -28,18 +48,18 @@ class PolynomialExtrapolator(object):
 
     @staticmethod
     def __get_poly_derivative(xs: np.array, betas: np.array, order: int = 1):
-        poly = np.poly1d(betas[::-1])
+        poly = np.poly1d(betas[::-1])  #
         derivative = np.polyder(poly, m=order)
         return derivative(xs)
 
     @staticmethod
-    def zeroth_order(xs: np.array, betas: np.array, lam: float, is_reciprocal: bool):
+    def zeroth_order(xs: np.array, betas: np.array, lam: float, is_reciprocal: bool) -> np.array:
         base = PolynomialExtrapolator.__get_base(xs, is_reciprocal)
         zeroth = np.power(base, lam) * np.exp(polyval(base, betas))
         return zeroth
 
     @staticmethod
-    def first_order(xs: np.array, betas: np.array, lam: float, is_reciprocal: bool):
+    def first_order(xs: np.array, betas: np.array, lam: float, is_reciprocal: bool) -> np.array:
         base = PolynomialExtrapolator.__get_base(xs, is_reciprocal)
         zeroth = PolynomialExtrapolator.zeroth_order(base, betas, lam, False)
         first = zeroth * (lam / base + PolynomialExtrapolator.__get_poly_derivative(base, betas, order=1))
@@ -48,7 +68,7 @@ class PolynomialExtrapolator(object):
         return first
 
     @staticmethod
-    def second_order(xs: np.array, betas: np.array, lam: float, is_reciprocal: bool):
+    def second_order(xs: np.array, betas: np.array, lam: float, is_reciprocal: bool) -> np.array:
         base = PolynomialExtrapolator.__get_base(xs, is_reciprocal)
         zeroth = PolynomialExtrapolator.zeroth_order(base, betas, lam, False)
         first = PolynomialExtrapolator.first_order(base, betas, lam, False)
--- test/Utils/Extrapolator/test_polynomialExtrapolator.py
+++ test/Utils/Extrapolator/test_polynomialExtrapolator.py
@@ -1,8 +1,12 @@
 from unittest import TestCase
 
+import matplotlib.pyplot as plt
 import numpy as np
 
+from src.SABRModel.SABRModel import SABRModelLognormalApprox
+from src.Utils.Black76 import Black76, Black76VecK
 from src.Utils.Extrapolator.PolynomialExtrapolator import PolynomialExtrapolator
+from src.Utils.OptionType import OptionType
 
 __author__ = 'frank.ma'
 
@@ -17,9 +21,10 @@ class TestPolynomialExtrapolator(TestCase):
         for is_reciprocal in [False, True]:
             xs_d = xs * (1.0 - dx)
             xs_u = xs * (1.0 + dx)
+            dxs = np.subtract(xs_u, xs_d)
             v_d = PolynomialExtrapolator.zeroth_order(xs_d, betas, lam, is_reciprocal)
             v_u = PolynomialExtrapolator.zeroth_order(xs_u, betas, lam, is_reciprocal)
-            first_n = (v_u - v_d) / (xs_u - xs_d)
+            first_n = (v_u - v_d) / dxs
             first = PolynomialExtrapolator.first_order(xs, betas, lam, is_reciprocal)
             for idx in range(xs.__len__()):
                 rel_diff = first[idx] / first_n[idx] - 1.0
@@ -36,10 +41,11 @@ class TestPolynomialExtrapolator(TestCase):
         for is_reciprocal in [False, True]:
             xs_d = xs * (1.0 - dx)
             xs_u = xs * (1.0 + dx)
+            dxs = np.subtract(xs_u, xs_d) / 2.0
             v_d = PolynomialExtrapolator.zeroth_order(xs_d, betas, lam, is_reciprocal)
             v = PolynomialExtrapolator.zeroth_order(xs, betas, lam, is_reciprocal)
             v_u = PolynomialExtrapolator.zeroth_order(xs_u, betas, lam, is_reciprocal)
-            second_n = (v_u - 2.0 * v + v_d) / ((0.5 * (xs_u - xs_d)) ** 2)
+            second_n = (v_u - 2.0 * v + v_d) / (dxs ** 2)
             second = PolynomialExtrapolator.second_order(xs, betas, lam, is_reciprocal)
             for idx in range(xs.__len__()):
                 rel_diff = second[idx] / second_n[idx] - 1.0
@@ -71,3 +77,87 @@ class TestPolynomialExtrapolator(TestCase):
             self.assertAlmostEqual(first[idx], first_rec[idx], places=12)
             self.assertAlmostEqual(second[idx], second_rec[idx], places=12)
         pass
+
+    def test_quadratic_fit_vs_wing(self):
+        f = 150.0
+        tau = 0.75
+        sig = 0.4
+        b = 0.96
+
+        for lam, opt_type in [(2.0, OptionType.put), (0.5, OptionType.call)]:
+            # for lam, opt_type in [(0.5, OptionType.call)]:
+            k = f / lam  # strikes should be on the wings
+            price = Black76.price(f, k, tau, sig, b, opt_type)
+            delta_k = Black76.delta_k(f, k, tau, sig, b, opt_type)
+            gamma_k = Black76.gamma_k(f, k, tau, sig, b)
+
+            pe = PolynomialExtrapolator.quadratic_fit_vs_wing(lam, k, price, delta_k, gamma_k, opt_type)
+            is_reciprocal = opt_type == OptionType.call
+            zeroth = pe.zeroth_order(k, pe.betas, lam, is_reciprocal)
+            first = pe.first_order(k, pe.betas, lam, is_reciprocal)
+            second = pe.second_order(k, pe.betas, lam, is_reciprocal)
+            self.assertAlmostEqual(price, zeroth, places=12)
+            self.assertAlmostEqual(delta_k, first, places=12)
+            self.assertAlmostEqual(gamma_k, second, places=12)
+
+    def test_extrapolate(self):
+
+        def __calc_derivatives(sabr_model: SABRModelLognormalApprox, forward: float, strike: float, ttm: float,
+                               opt_type: OptionType, dk: float = 1e-2):
+            strike_m = strike * (1.0 - dk)
+            strike_p = strike * (1.0 + dk)
+            vol_m = sabr_model.calc_vol(forward, strike_m)
+            vol = sabr_model.calc_vol(forward, strike)
+            vol_p = sabr_model.calc_vol(forward, strike_p)
+            price_m = Black76.price(forward, strike_m, ttm, vol_m, 1.0, opt_type)
+            price = Black76.price(forward, strike, ttm, vol, 1.0, opt_type)
+            price_p = Black76.price(forward, strike_p, ttm, vol_p, 1.0, opt_type)
+            delta_k = (price_p - price_m) / (strike_p - strike_m)
+            gamma_k = (price_p - 2.0 * price + price_m) / ((strike * dk) ** 2)
+            return price, delta_k, gamma_k
+
+        f = 97.4
+        tau = 5.0
+        b = 1.0
+        k_flr, k_left, k_right, k_cap = 5.0, 80.0, 200.0, 300.0
+        alpha, beta, nu, rho = 0.12, 1.0, 0.88, -0.66
+
+        ks_full = np.linspace(k_flr, k_cap, num=int(k_cap - k_flr))
+        ks_left = np.linspace(k_flr, k_left, num=int(k_left - k_flr))
+        ks_right = np.linspace(k_right, k_cap, num=int(k_cap - k_right))
+
+        cases = [(OptionType.put, 2.33), (OptionType.put, 5.0), (OptionType.put, 15.0),
+                 (OptionType.call, 2.33), (OptionType.call, 5.0), (OptionType.call, 15.0)]
+        model = SABRModelLognormalApprox(tau, alpha, beta, nu, rho)
+        vols_ful = model.calc_vol_vec(f, ks_full)
+
+        plt.subplot(2, 1, 1)
+        plt.title('implied vol')
+        plt.plot(ks_full, vols_ful)
+        legends = ['SABR']
+
+        for opt_type, lam in cases:
+            k = k_left if opt_type == OptionType.put else k_right
+            ks = ks_left if opt_type == OptionType.put else ks_right
+            zeroth, first, second = __calc_derivatives(model, f, k, tau, opt_type)
+            pe = PolynomialExtrapolator.quadratic_fit_vs_wing(lam, k, zeroth, first, second, opt_type)
+            prices = pe.extrapolate(ks)
+            vols = Black76VecK.imp_vol(f, ks, tau, prices, b, opt_type)
+            plt.plot(ks, vols)
+            legends += [opt_type.name + '_' + lam.__str__()]
+        plt.legend(legends)
+
+        plt.subplot(2, 1, 2)
+        plt.title('density')
+        dens_full, ks_den_ful = model.calc_fwd_den(f, rel_bounds=(k_flr / f, k_cap / f))
+        plt.plot(ks_den_ful, dens_full)
+
+        for opt_type, lam in cases:
+            k = k_left if opt_type == OptionType.put else k_right
+            ks = ks_left if opt_type == OptionType.put else ks_right
+            zeroth, first, second = __calc_derivatives(model, f, k, tau, opt_type)
+            pe = PolynomialExtrapolator.quadratic_fit_vs_wing(lam, k, zeroth, first, second, opt_type)
+            dens = pe.second_order(ks, pe.betas, lam, opt_type == OptionType.call)
+            plt.plot(ks, dens)
+        plt.legend(legends)
+        plt.show()
