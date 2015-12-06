002f1b2f7b5b4603a68fa0e518d2fd86ba3f80e5
Frank Ma <guang.y.ma@gmail.com>
2015-12-06 14:08:38 -0500

    add vol to strike method to FX quote converter

--- src/Utils/FXQuotesConverter.py
+++ src/Utils/FXQuotesConverter.py
@@ -1,5 +1,11 @@
 import numpy as np
 
+from src.Utils.BSM import BSM
+from src.Utils.OptionType import OptionType
+from src.Utils.Solver.Brent import Brent
+from src.Utils.Solver.IVariateFunction import IUnivariateFunction
+from src.Utils.Solver.NewtonRaphson import NewtonRaphson
+
 __author__ = 'frank.ma'
 
 
@@ -27,3 +33,31 @@ class FXQuotesConverter(object):
         quotes_vec = np.array([rr_10, rr_25, atm, sm_25, sm_10])
         vols_vec = np.linalg.solve(mtx, quotes_vec)
         return vols_vec
+
+    @staticmethod
+    def vol_to_strike(sig: float, delta: float, tau: float, spot: float, rate_dom: float, rate_for: float,
+                      method='Newton-Raphson'):
+        opt_type = OptionType.call if delta > 0.0 else OptionType.put
+
+        class DeltaFunction(IUnivariateFunction):
+            def evaluate(self, x):
+                return BSM.delta(spot, x, tau, rate_dom, rate_for, sig, opt_type) - delta
+
+        class DDeltaDKFunction(IUnivariateFunction):
+            def evaluate(self, x):
+                gamma = BSM.gamma(spot, x, tau, rate_dom, rate_for, sig)
+                return -gamma * spot / x  # d^2 v / ds^2 is close to d^2 v / dk ds with correction term - s / x
+
+        zeroth = DeltaFunction()
+        first = DDeltaDKFunction()
+
+        if method == 'Brent':
+            bt = Brent(zeroth, 1e-4 * spot, 10.0 * spot)
+            strike = bt.solve()
+        elif method == 'Newton-Raphson':
+            nr = NewtonRaphson(zeroth, first, spot)
+            strike = nr.solve()
+        else:
+            raise ValueError('Unrecognized optimization method %s.' % method)
+
+        return strike
--- test/Utils/testFXQuotesConverter.py
+++ test/Utils/testFXQuotesConverter.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
 
+from src.Utils.BSM import BSM
 from src.Utils.FXQuotesConverter import FXQuotesConverter
+from src.Utils.OptionType import OptionType
 
 __author__ = 'frank.ma'
 
@@ -17,9 +19,28 @@ class TestFXQuotesConverter(TestCase):
 
         res = FXQuotesConverter.read_quotes(quotes)
 
-        self.assertAlmostEqual(sig_10, res[0], places=12, msg='delta 0.9 par failed')
-        self.assertAlmostEqual(sig_25, res[1], places=12, msg='delta 0.9 par failed')
-        self.assertAlmostEqual(sig_50, res[2], places=12, msg='delta 0.9 par failed')
-        self.assertAlmostEqual(sig_75, res[3], places=12, msg='delta 0.9 par failed')
-        self.assertAlmostEqual(sig_90, res[4], places=12, msg='delta 0.9 par failed')
+        self.assertAlmostEqual(sig_10, res[0], places=12, msg='delta 0.10 conversion failed')
+        self.assertAlmostEqual(sig_25, res[1], places=12, msg='delta 0.25 conversion failed')
+        self.assertAlmostEqual(sig_50, res[2], places=12, msg='delta 0.50 conversion failed')
+        self.assertAlmostEqual(sig_75, res[3], places=12, msg='delta 0.75 conversion failed')
+        self.assertAlmostEqual(sig_90, res[4], places=12, msg='delta 0.90 conversion failed')
         pass
+
+    def test_vol_to_strike(self):
+        spot = 150.0
+        taus = [0.05, 0.1, 0.2, 0.5, 0.75, 1.0]
+        rate_dom = 0.04
+        rate_for = 0.02
+        strikes = [50.0, 100.0, 150.0, 200.0, 250.0]
+        # strikes = [50.0]
+        vols = [0.3, 0.25, 0.2, 0.21, 0.26]
+
+        for tau in taus:
+            for kdx, strike in enumerate(strikes):
+                vol = vols[kdx]
+                opt_type = OptionType.call if strike > spot else OptionType.put  # OTM quotes convert much faster
+                delta = BSM.delta(spot, strike, tau, rate_dom, rate_for, vol, opt_type=opt_type)
+                strike_rep = FXQuotesConverter.vol_to_strike(vol, delta, tau, spot, rate_dom, rate_for)
+                self.assertAlmostEqual(strike, strike_rep, places=12,
+                                       msg='vol %.4f to strike %.2f at delta %.12f conversion failed at tau %.4f'
+                                           % (vol, strike, delta, tau))
