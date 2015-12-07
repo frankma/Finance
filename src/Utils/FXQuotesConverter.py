201b1a59c679e3a49d97ef65deff38c29851244d
Frank Ma <guang.y.ma@gmail.com>
2015-12-07 07:39:39 -0500

    update FX quotes converter

--- src/Utils/FXQuotesConverter.py
+++ src/Utils/FXQuotesConverter.py
@@ -10,15 +10,32 @@ __author__ = 'frank.ma'
 
 
 class FXQuotesConverter(object):
-    def __init__(self, tau: float, spot: float, rate_dom: float, rate_for: float, quotes: dict):
+    def __init__(self, spot: float, tau: float, rate_dom: float, rate_for: float, quotes: dict):
         self.tau = tau
         self.spot = spot
         self.rate_dom = rate_dom
         self.rate_for = rate_for
         self.quotes = quotes
+        self.strikes = None
+        self.vols = None
+
+    def convert(self, method: str = 'Newton-Raphson'):
+        keys = ['rr_10', 'rr_25', 'atm_50', 'sm_25', 'sm_10']
+        deltas = [-0.1, -0.25, 0.5, 0.25, 0.1]
+        if not all(key in self.quotes for key in keys):
+            raise ValueError('all keys of quote set %s are required' % keys.__str__())
+
+        vols = self.read_quotes(self.quotes)
+        ks = np.zeros(np.shape(vols), dtype=float)
+        for kdx, delta in enumerate(deltas):
+            ks[kdx] = self.vol_to_strike(vols[kdx], delta, self.tau, self.spot, self.rate_dom, self.rate_for,
+                                         method=method)
+        self.strikes = ks
+        self.vols = vols
+        return ks, vols
 
     @staticmethod
-    def read_quotes(quotes):
+    def read_quotes(quotes, seven_quotes: bool = False):
         rr_10 = quotes['rr_10']
         rr_25 = quotes['rr_25']
         atm = quotes['atm_50']
@@ -31,6 +48,19 @@ class FXQuotesConverter(object):
                         [0.0, 0.5, -1.0, 0.5, 0.0],
                         [0.5, 0.0, -1.0, 0.0, 0.5]])
         quotes_vec = np.array([rr_10, rr_25, atm, sm_25, sm_10])
+
+        if seven_quotes:
+            mtx = np.array([[-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+                            [0.0, -1.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+                            [0.0, 0.0, -1.0, 0.0, 1.0, 0.0, 0.0],
+                            [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
+                            [0.0, 0.0, 0.5, -1.0, 0.5, 0.0, 0.0],
+                            [0.0, 0.5, 0.0, -1.0, 0.0, 0.5, 0.0],
+                            [0.5, 0.0, 0.0, -1.0, 0.0, 0.0, 0.5]])
+            rr_05 = quotes['rr_05']
+            sm_05 = quotes['sm_05']
+            quotes_vec = np.array([rr_05, rr_10, rr_25, atm, sm_25, sm_10, sm_05])
+
         vols_vec = np.linalg.solve(mtx, quotes_vec)
         return vols_vec
 
--- test/Utils/testFXQuotesConverter.py
+++ test/Utils/testFXQuotesConverter.py
@@ -24,6 +24,20 @@ class TestFXQuotesConverter(TestCase):
         self.assertAlmostEqual(sig_50, res[2], places=12, msg='delta 0.50 conversion failed')
         self.assertAlmostEqual(sig_75, res[3], places=12, msg='delta 0.75 conversion failed')
         self.assertAlmostEqual(sig_90, res[4], places=12, msg='delta 0.90 conversion failed')
+
+        sig_05 = 0.50
+        sig_95 = 0.40
+        quotes.update({'rr_05': sig_95 - sig_05,
+                       'sm_05': 0.5 * (sig_05 - 2.0 * sig_50 + sig_95)})
+        res = FXQuotesConverter.read_quotes(quotes, seven_quotes=True)
+
+        self.assertAlmostEqual(sig_05, res[0], places=12, msg='delta 0.05 conversion failed')
+        self.assertAlmostEqual(sig_10, res[1], places=12, msg='delta 0.10 conversion failed')
+        self.assertAlmostEqual(sig_25, res[2], places=12, msg='delta 0.25 conversion failed')
+        self.assertAlmostEqual(sig_50, res[3], places=12, msg='delta 0.50 conversion failed')
+        self.assertAlmostEqual(sig_75, res[4], places=12, msg='delta 0.75 conversion failed')
+        self.assertAlmostEqual(sig_90, res[5], places=12, msg='delta 0.90 conversion failed')
+        self.assertAlmostEqual(sig_95, res[6], places=12, msg='delta 0.95 conversion failed')
         pass
 
     def test_vol_to_strike(self):
@@ -43,4 +57,22 @@ class TestFXQuotesConverter(TestCase):
                 self.assertAlmostEqual(strike, strike_rep, places=12,
                                        msg='vol %.4f to strike %.2f at delta %.12f conversion failed at tau %.4f'
                                            % (vol, strike, delta, tau))
+        pass
+
+    def test_convert(self):
+        spot = 1.10265
+        tau = 0.498288843
+        rate_dom = 0.00262834
+        rate_for = -0.005218988
+        quotes = {'rr_10': -0.030858411,
+                  'rr_25': -0.018367746,
+                  'atm_50': 0.103690639,
+                  'sm_25': 0.002870256,
+                  'sm_10': 0.008569103}
+
+        converter = FXQuotesConverter(spot, tau, rate_dom, rate_for, quotes)
+        strikes, vols = converter.convert()
+
+        print(strikes)
+        print(vols)
         pass
