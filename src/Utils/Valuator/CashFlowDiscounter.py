39c6e91b88cc1f5001cd95f660d8c0a844b26003
Frank Ma <guang.y.ma@gmail.com>
2016-01-29 00:01:33 -0500

    add non-parametrized market rate to zero rate converter

---
+++ src/Utils/Valuator/CashFlowDiscounter.py
@@ -0,0 +1,40 @@
+import logging
+import numpy as np
+
+from src.Utils.Solver.Brent import Brent
+from src.Utils.Solver.IVariateFunction import IUnivariateFunction
+
+__author__ = 'frank.ma'
+
+logger = logging.getLogger()
+
+
+class CashFlowDiscounter(object):
+    def __init__(self, taus: np.array, cash_flows: np.array):
+        self.ts = taus
+        self.cs = cash_flows
+
+    @staticmethod
+    def calc_present_values(taus: np.array, cash_flows: np.array, rates: np.array):
+        return np.exp(-taus * rates) * cash_flows
+
+    def calc_npv(self, rates: float or np.array):
+        cash_flows_discounted = self.calc_present_values(self.ts, self.cs, rates)
+        return np.sum(cash_flows_discounted)
+
+    def calc_irr(self, lb: float = -0.1, ub: float = 2.0):
+        # this is an internal wrapper class for the root search
+        class _NPV(IUnivariateFunction):
+            def __init__(self, taus: np.array, cash_flows: np.array):
+                self.taus = taus
+                self.cash_flows = cash_flows
+                pass
+
+            def evaluate(self, x):
+                return np.sum(CashFlowDiscounter.calc_present_values(self.taus, self.cash_flows, x))
+
+        tgt_func = _NPV(self.ts, self.cs)
+        solver = Brent(tgt_func, lb, ub)
+        irr = solver.solve()
+
+        return irr
---
+++ test/Utils/Valuator/test_CashFlowDiscounter.py
@@ -0,0 +1,67 @@
+import logging
+import sys
+from unittest import TestCase
+
+import numpy as np
+
+from src.Utils.Valuator.CashFlowDiscounter import CashFlowDiscounter
+
+__author__ = 'frank.ma'
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+sh = logging.StreamHandler()
+sh.setLevel(logging.DEBUG)
+sh.setFormatter(formatter)
+logger.addHandler(sh)
+
+
+class TestCashFlowDiscounter(TestCase):
+    def test_calc_present_values(self):
+        logger.info('%s starts' % sys._getframe().f_code.co_name)
+        # case of zero discount rate and zero npv
+        taus = np.array([0.0, 1.0])
+        cash_flows = np.array([-1.0, 1.0])
+        npv = sum(CashFlowDiscounter.calc_present_values(taus, cash_flows, 0.0))
+        self.assertAlmostEqual(0.0, npv, places=12)
+        # case of none-zero discount rate and zero npv
+        rate = -0.05
+        taus = np.array([0.0, 1.0])
+        cash_flows = np.array([-np.exp(-rate * 1.0), 1.0])
+        npv = sum(CashFlowDiscounter.calc_present_values(taus, cash_flows, rate))
+        self.assertAlmostEqual(0.0, npv, places=12)
+        # case of none-zero discount rate and zero npb
+        rate = 0.05
+        taus = np.array([0.0, 1.0])
+        cash_flows = np.array([-np.exp(-rate * 1.0), 1.0])
+        npv = sum(CashFlowDiscounter.calc_present_values(taus, cash_flows, rate))
+        self.assertAlmostEqual(0.0, npv, places=12)
+        logger.info('%s passes' % sys._getframe().f_code.co_name)
+        pass
+
+    def test_calc_irr(self):
+        logger.info('%s starts' % sys._getframe().f_code.co_name)
+        # case of zero rate
+        rate = 0.0
+        taus = np.array([0.0, 1.0, 2.0])
+        cash_flows = np.array([-2.0, 1.0, 1.0])
+        cfd = CashFlowDiscounter(taus, cash_flows)
+        irr = cfd.calc_irr()
+        self.assertAlmostEqual(rate, irr, places=6)
+        # case of negative rate
+        rate = -0.05
+        taus = np.array([0.0, 1.0, 2.0])
+        cash_flows = np.array([-2.0, 1.0 * np.exp(rate * 1.0), 1.0 * np.exp(rate * 2.0)])
+        cfd = CashFlowDiscounter(taus, cash_flows)
+        irr = cfd.calc_irr()
+        self.assertAlmostEqual(rate, irr, places=6)
+        # case of negative rate
+        rate = 0.05
+        taus = np.array([0.0, 1.0, 2.0])
+        cash_flows = np.array([-2.0, 1.0 * np.exp(rate * 1.0), 1.0 * np.exp(rate * 2.0)])
+        cfd = CashFlowDiscounter(taus, cash_flows)
+        irr = cfd.calc_irr()
+        self.assertAlmostEqual(rate, irr, places=6)
+        logger.info('%s passes' % sys._getframe().f_code.co_name)
