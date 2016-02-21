5e78f063827ff51fcc74a01889d545d230957dd7
Frank Ma <guang.y.ma@gmail.com>
2016-02-20 23:13:45 -0500

    minor work in vol crush

--- src/MiniProjects/VolatilityCrush/main.py
+++ src/MiniProjects/VolatilityCrush/main.py
@@ -2,12 +2,23 @@ from datetime import datetime, timedelta
 
 __author__ = 'frank.ma'
 
-asof = datetime(2016, 2, 12)
-event = datetime(2016, 2, 17)
+dt_pre = datetime(2016, 2, 16)
+dt_event = datetime(2016, 2, 17)
+dt_post = datetime(2016, 2, 18)
 
 exp_s = datetime(2016, 2, 19)
 exp_l = datetime(2016, 3, 18)
 
-tau_s = (exp_s - asof) / timedelta(365.25)
-tau_l = (exp_l - asof) / timedelta(365.25)
-print(tau_s, tau_l)
+tau_s = (exp_s - dt_pre) / timedelta(365.25)
+tau_l = (exp_l - dt_pre) / timedelta(365.25)
+
+r = 0.05
+q = 0.03
+
+sig_s = 0.55
+sig_l = 0.45
+
+strike = 155.0
+spot_pre = 150.0
+spot_post = 170.0
+
