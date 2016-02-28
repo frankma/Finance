21524f4863e58d97d7152601ca09e6d12d79d03a
Frank Ma <guang.y.ma@gmail.com>
2016-02-28 13:12:31 -0500

    placeholder for vol crush

--- src/Algorithms/VolatilityCrush/main.py
+++ src/Algorithms/VolatilityCrush/main.py
@@ -2,15 +2,17 @@ from datetime import datetime, timedelta
 
 __author__ = 'frank.ma'
 
-dt_pre = datetime(2016, 2, 16)
-dt_event = datetime(2016, 2, 17)
-dt_post = datetime(2016, 2, 18)
+dt_pre = datetime(2016, 2, 16, 16, 0, 0)
+dt_event = datetime(2016, 2, 17, 16, 0, 0)
+dt_post = datetime(2016, 2, 18, 16, 0, 0)
 
 exp_s = datetime(2016, 2, 19)
 exp_l = datetime(2016, 3, 18)
 
 tau_s = (exp_s - dt_pre) / timedelta(365.25)
 tau_l = (exp_l - dt_pre) / timedelta(365.25)
+
+print(tau_s, tau_l)
 
 r = 0.05
 q = 0.03
