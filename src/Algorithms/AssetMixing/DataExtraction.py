cff982a9029d56e81724e87bb190855fd2cea172
Frank Ma <guang.y.ma@gmail.com>
2016-03-18 08:14:37 -0400

    minor improvements

--- src/Algorithms/AssetMixing/DataExtraction.py
+++ src/Algorithms/AssetMixing/DataExtraction.py
@@ -4,6 +4,11 @@ __author__ = 'frank.ma'
 
 
 class DataExtraction(DataSource):
-
     def __init__(self):
         super.__init__()
+
+    def raw_data(self):
+        pass
+
+    def instance_hash(self):
+        pass
