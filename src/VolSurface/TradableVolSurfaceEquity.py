f5dfe7fb60c7f7a229aae3f6c7c7b08cb8e1a187
Frank Ma <guang.y.ma@gmail.com>
2016-04-06 07:54:45 -0400

    minor updates

--- src/VolSurface/TradableVolSurfaceEquity.py
+++ src/VolSurface/TradableVolSurfaceEquity.py
@@ -1,6 +1,6 @@
 import logging
-from datetime import datetime
-import numpy as np
+
+import pandas as pd
 
 from src.Quote.QuoteVanillaOptionEquity import QuoteVanillaOptionEquity
 from src.Utils.Types.PayoffType import PayoffType
@@ -15,27 +15,62 @@ class TradableVolSurfaceEquity(ITradableVolSurface):
     def __init__(self, quotes: list):
         # screening the quote set first
         asof = None
+        payoff_type = None
         self.opt_eur = []
         self.opt_ame = []
         for idx, quote in enumerate(quotes):
             if not isinstance(quote, QuoteVanillaOptionEquity):
-                raise AttributeError('expect object type of %s, received object type of %s at position % i'
-                                     % (QuoteVanillaOptionEquity.__name__, quote.__class__.__name__, idx))
+                logger.warning('expect object type of %s, received object type of %s at position %i; bypass it'
+                               % (QuoteVanillaOptionEquity.__name__, quote.__class__.__name__, idx))
+                continue
             if idx == 0:
-                asof = quote.asof  # assume first one is baseline
+                # always take the first one as baseline
+                asof = quote.asof
+                payoff_type = quote.payoff_type
             else:
                 if not asof.__eq__(quote.asof):
-                    raise ValueError('expect asof of %s, received misaligned asof of %s at position % i'
-                                     % (asof, quote.asof, idx))
+                    logger.warning('expect asof %s, received asof %s at position %i; bypass it'
+                                   % (asof, quote.asof, idx))
+                    continue
+                if quote.opt_type is not payoff_type:
+                    logger.warning('expect payoff type of %s, received payoff type %s at position %i; bypass it'
+                                   % (payoff_type.name, quote.payoff_type.name, idx))
+                    continue
+            q = [quote.asof, quote.expiry, quote.strike, quote.opt_type, quote.bid, quote.ask]
             if quote.payoff_type is PayoffType.European:
-                self.opt_eur.append(quote)
+                self.opt_eur.append(q)
             elif quote.payoff_type is PayoffType.American:
-                self.opt_ame.append(quote)
+                self.opt_ame.append(q)
             else:
                 logger.warning('payoff type %s could not be handled, bypass the quote' % quote.payoff_type)
 
         super().__init__(asof)
         self.quotes = quotes
 
+    @staticmethod
+    def __create_df(quote_table, columns=None, id_contact=None):
+        # give defaults to mutable inputs
+        if columns is None:
+            columns = ['asof', 'expiry', 'strike', 'opt_type', 'bid', 'ask']
+        if id_contact is None:
+            id_contact = ['asof', 'expiry', 'strike', 'opt_type']
+
+        # create data frame and drop duplicates
+        df = pd.DataFrame(quote_table, columns=columns)
+        df['id'] = df.apply(lambda x: '_'.join(x[id_contact].astype(str)), axis=1)
+        df = df.drop_duplicates(subset=['id'])
+        return df
+
     def clean(self):
+        # firstly European style options
+        df_eur = self.__create_df(self.opt_eur)
+
+        # secondly American style options
+        df_ame = self.__create_df(self.opt_ame)
+
+        # lastly, if european available, try to imply forward
+        if df_eur.__len__() > 2:
+            # do something
+            pass
+
         pass
---
+++ test/VolSurface/test_TradableVolSurfaceEquity.py
@@ -0,0 +1,23 @@
+import logging
+import sys
+
+from unittest import TestCase
+
+__author__ = 'frank.ma'
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+sh = logging.StreamHandler()
+sh.setLevel(logging.DEBUG)
+sh.setFormatter(formatter)
+logger.addHandler(sh)
+
+
+class TestTradableVolSurfaceEquity(TestCase):
+    def test_sample(self):
+        logger.info('%s starts' % sys._getframe().f_code.co_name)
+
+        logger.info('%s passes' % sys._getframe().f_code.co_name)
+        pass
