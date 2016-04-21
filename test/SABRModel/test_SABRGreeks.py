04645667cea1e3c93a2032595151641960133d90
Frank Ma <guang.y.ma@gmail.com>
2016-04-20 20:47:22 -0400

    minor update to sabr greek test

--- test/SABRModel/test_SABRGreeks.py
+++ test/SABRModel/test_SABRGreeks.py
@@ -47,6 +47,7 @@ class TestSABRGreeks(TestCase):
 
     def test_pde_f(self):
         logger.info('%s starts' % sys._getframe().f_code.co_name)
+
         forward = 150.0
         strike = 145.0
         tau = 1.75
@@ -70,6 +71,24 @@ class TestSABRGreeks(TestCase):
 
     def test_pde_k(self):
         logger.info('%s starts' % sys._getframe().f_code.co_name)
+
+        forward = 150.0
+        strike = 145.0
+        tau = 1.75
+        b = 1.0
+        alpha, beta, nu, rho = 0.2, 1.0, 0.4, -0.33
+        model = SABRModelLognormalApprox(tau, alpha, beta, nu, rho)
+        opt_type = OptionType.call
+        sigma = model.calc_vol(forward, strike)
+        theta_sabr = SABRGreeks.theta(forward, strike, tau, b, opt_type, model)
+        theta_black = Black76.theta(forward, strike, tau, sigma, b, opt_type)
+        logger.info('THETA sabr %.6f black %.6f' % (theta_sabr, theta_black))
+        delta_k_sabr = SABRGreeks.delta_k(forward, strike, tau, b, opt_type, model)
+        delta_k_black = Black76.delta_k(forward, strike, tau, sigma, b, opt_type)
+        logger.info('DELTA_K sabr %.6f black %.6f' % (delta_k_sabr, delta_k_black))
+        gamma_k_sabr = SABRGreeks.gamma_k(forward, strike, tau, b, model)
+        gamma_k_black = Black76.gamma_k(forward, strike, tau, sigma, b)
+        logger.info('GAMMA_K sabr %.6f black %.6f' % (gamma_k_sabr, gamma_k_black))
 
         logger.info('%s passes' % sys._getframe().f_code.co_name)
         pass
