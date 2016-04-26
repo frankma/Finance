43030a075105ea88c18858a5877acc743c4c357e
Frank Ma <guang.y.ma@gmail.com>
2016-04-26 07:52:02 -0400

    add radom bivariate pdf function

--- src/Utils/Sequence/RdmBivariate.py
+++ src/Utils/Sequence/RdmBivariate.py
@@ -11,5 +11,9 @@ class RdmBivariate(object):
         return rho * x1 + np.sqrt(1.0 - rho * rho) * x2
 
     @staticmethod
-    def cdf(x1: np.array, x2: np.array, rho: float):
-        pass
+    def cdf(x_1: np.array, x_2: np.array, rho: float, mu_1: float = 0.0, mu_2: float = 0.0,
+            sig_1: float = 1.0, sig_2: float = 1.0):
+        x_1_norm = (x_1 - mu_1) / sig_1
+        x_2_norm = (x_2 - mu_2) / sig_2
+        z = (x_1_norm ** 2) - 2.0 * rho * x_1_norm * x_2_norm + (x_2_norm ** 2)
+        return np.exp(-z / (2.0 * np.sqrt(1.0 - rho ** 2))) / (2.0 * np.pi * sig_1 * sig_2 * np.sqrt(1.0 - rho ** 2))
