338a4701c971d26af40f70697f9717b5c8caee98
Frank Ma <guang.y.ma@gmail.com>
2016-04-28 07:48:56 -0400

    update to rdm bivar

--- src/Utils/Sequence/RdmBivariate.py
+++ src/Utils/Sequence/RdmBivariate.py
@@ -8,17 +8,23 @@ logger = logging.getLogger(__name__)
 
 class RdmBivariate(object):
     @staticmethod
-    def draw(rho: float, size: int):
+    def __check_rho(rho: float):
+        if abs(rho) >= 1.0:
+            raise ValueError('rho (%.4f) should be smaller than 1' % rho)
+
+    @staticmethod
+    def draw_std(rho: float, size: int):
+        RdmBivariate.__check_rho(rho)
         x1 = np.random.random(size=size)
         x2 = np.random.random(size=size)
-        return rho * x1 + np.sqrt(1.0 - rho * rho) * x2
+        return x1, rho * x1 + np.sqrt(1.0 - rho * rho) * x2
 
     @staticmethod
     def pdf(x_1: np.array, x_2: np.array, rho: float, mu_1: float = 0.0, mu_2: float = 0.0,
             sig_1: float = 1.0, sig_2: float = 1.0):
-        if abs(rho) >= 1.0:
-            raise ValueError('rho (%.4f) should be smaller than 1' % rho)
+        RdmBivariate.__check_rho(rho)
         x_1_norm = (x_1 - mu_1) / sig_1
         x_2_norm = (x_2 - mu_2) / sig_2
-        z = (x_1_norm ** 2) - 2.0 * rho * x_1_norm * x_2_norm + (x_2_norm ** 2)
+        xx1, xx2 = np.meshgrid(x_1_norm, x_2_norm)
+        z = (xx1 ** 2) - 2.0 * rho * xx1 * xx2 + (xx2 ** 2)
         return np.exp(-z / (2.0 * np.sqrt(1.0 - rho ** 2))) / (2.0 * np.pi * sig_1 * sig_2 * np.sqrt(1.0 - rho ** 2))
--- test/Utils/Sequence/test_RdmBivariate.py
+++ test/Utils/Sequence/test_RdmBivariate.py
@@ -6,6 +6,7 @@ from unittest import TestCase
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+
 from src.Utils.Sequence.RdmBivariate import RdmBivariate
 
 __author__ = 'frank.ma'
@@ -21,16 +22,22 @@ logger.addHandler(sh)
 
 
 class TestRdmBivariate(TestCase):
-    def test_draw(self):
+    def test_draw_std(self):
         logger.info('%s starts' % sys._getframe().f_code.co_name)
+        rho = 0.2
+        size = 10 ** 6
+        x1, x2 = RdmBivariate.draw_std(rho=rho, size=size)
+        cor = np.corrcoef(x1, x2)[(0, 1)]
+        logger.debug('rho input: %.6f, simulation output: %.6f' % (rho, cor))
+        self.assertAlmostEqual(rho, cor, places=2)
         logger.info('%s passes' % sys._getframe().f_code.co_name)
         pass
 
     def test_pdf(self):
         logger.info('%s starts' % sys._getframe().f_code.co_name)
-        x_1 = np.linspace(-10, 10)
-        x_2 = np.linspace(-10, 10)
-        pdf = RdmBivariate.pdf(x_1, x_2, rho=0.1)
+        x_1 = np.linspace(-5, 5)
+        x_2 = np.linspace(-5, 5)
+        pdf = RdmBivariate.pdf(x_1, x_2, rho=-0.45)
         xx_1, xx_2 = np.meshgrid(x_1, x_2)
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
