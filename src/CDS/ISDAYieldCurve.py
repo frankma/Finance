05a8852d66f560b283ba641496e49b5b98629969
Frank Ma <guang.y.ma@gmail.com>
2016-05-17 00:00:29 -0400

    minor update

--- src/CDS/ISDAYieldCurve.py
+++ src/CDS/ISDAYieldCurve.py
@@ -1,1 +1,17 @@
+import logging
+from datetime import datetime
+
 __author__ = 'frank.ma'
+
+logger = logging.getLogger(__name__)
+
+
+class ISDAYieldCurve(object):
+    def __init__(self, asof: datetime, tenors: list, rates: list):
+        self.asof = asof
+        pass
+
+    def calc_df(self, date: datetime):
+        if date < self.asof:
+            raise ValueError('request time %s is before asof date %s' % (date, self.asof))
+        pass
