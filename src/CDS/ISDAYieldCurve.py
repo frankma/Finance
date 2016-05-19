d11138299f0c7a4cfc242caf1ec4fe40a832f3ad
Frank Ma <guang.y.ma@gmail.com>
2016-05-18 22:06:19 -0400

    minor update

--- src/CDS/ISDAYieldCurve.py
+++ src/CDS/ISDAYieldCurve.py
@@ -9,9 +9,12 @@ logger = logging.getLogger(__name__)
 class ISDAYieldCurve(object):
     def __init__(self, asof: datetime, tenors: list, rates: list):
         self.asof = asof
+        self.tenors = tenors
+        self.rates = rates
         pass
 
     def calc_df(self, date: datetime):
         if date < self.asof:
             raise ValueError('request time %s is before asof date %s' % (date, self.asof))
+
         pass
