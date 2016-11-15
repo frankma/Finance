97fd561a2a49afffcb2493ba314126269650fbfe
Frank Ma <guang.y.ma@gmail.com>
2016-11-14 20:02:30 -0500

    add vega term

--- src/MiniProjects/DeltaGammaVega/main.py
+++ src/MiniProjects/DeltaGammaVega/main.py
@@ -18,12 +18,14 @@ price = BSM.price(s, k, tau, r, q, sig, opt_type)
 bump = 0.01
 
 s_d = s * (1. - bump)
-s_u = s * (1 + bump)
+s_u = s * (1. + bump)
+sig_d = sig * (1. - bump)
+sig_u = sig * (1. + bump)
 
 p_d_s = BSM.price(s_d, k, tau, r, q, sig, opt_type)
 p_u_s = BSM.price(s_u, k, tau, r, q, sig, opt_type)
-p_d_v = BSM.price(s, k, tau, r, q, sig * (1.0 - bump), opt_type)
-p_u_v = BSM.price(s, k, tau, r, q, sig * (1.0 + bump), opt_type)
+p_d_v = BSM.price(s, k, tau, r, q, sig_d, opt_type)
+p_u_v = BSM.price(s, k, tau, r, q, sig_u, opt_type)
 
 delta_num = (p_u_s - p_d_s) / (2. * s * bump)
 delta = BSM.delta(s, k, tau, r, q, sig, opt_type)
@@ -37,28 +39,38 @@ print('%6f\t%6f' % (delta_num, delta))
 print('%6f\t%6f' % (gamma_num, gamma))
 print('%6f\t%6f' % (vega_num, vega))
 
-s_vec = np.array(np.linspace(0.1 * s, 3.0 * s, num=100))
+s_vec = np.array(np.linspace(0.1 * s, 1.9 * s, num=100))
 price_s_vec = BSMVec.price(s_vec, k, tau, r, q, sig, opt_type)
 pnl_s = price_s_vec - price
 s_ret = s_vec / s - 1.0
 pnl_rep_dg = delta_num * s_ret * 100. + 0.5 * gamma_num * s_ret * s_ret * 10000.
 
-vol_vec = np.array(np.linspace(0.99 * sig, 1.01 * sig, num=100))
-price_s_vec = BSMVec.price(s, k, tau, r, q, vol_vec, opt_type)
+sig_vec = np.array(np.linspace(0.1 * sig, 2.0 * sig, num=100))
+price_s_vec = BSMVec.price(s, k, tau, r, q, sig_vec, opt_type)
 pnl_v = price_s_vec - price
-v_ret = vol_vec / sig - 1.0
-pnl_rep_v = vega * v_ret * 100.0
+sig_ret = sig_vec / sig - 1.0
+pnl_rep_v = vega_num * sig_ret * sig
 
-plt.plot(v_ret, pnl_v, '+-', label='PnL_v')
-plt.plot(v_ret, pnl_rep_v, 'x-', label='PnL Rep Vega')
-plt.plot(v_ret, np.zeros(vol_vec.shape))
+plt.subplot(2, 2, 1)
+plt.plot(s_ret, pnl_s, '.-', label='PnL')
+plt.plot(s_ret, pnl_rep_dg, '.-', label='PnL Rep')
+plt.plot(s_ret, np.zeros(s_vec.shape))
 plt.legend()
 plt.grid()
-plt.show()
 
-# plt.plot(s_ret, pnl_s, '+-', label='PnL')
-# plt.plot(s_ret, pnl_rep_dg, 'x-', label='PnL Rep')
-# plt.plot(s_ret, np.zeros(s_vec.shape))
-# plt.legend()
-# plt.grid(True)
-# plt.show()
+plt.subplot(2, 2, 3)
+plt.plot(s_ret, pnl_s - pnl_rep_dg, '-', label='residual')
+plt.legend()
+plt.grid()
+
+plt.subplot(2, 2, 2)
+plt.plot(sig_ret, pnl_v, '.-', label='PnL_v')
+plt.plot(sig_ret, pnl_rep_v, '.-', label='PnL Rep Vega')
+plt.plot(sig_ret, np.zeros(sig_vec.shape))
+plt.legend()
+plt.grid()
+
+plt.subplot(2, 2, 4)
+plt.plot(sig_ret, pnl_v - pnl_rep_v, '-', label='residual')
+plt.grid()
+plt.show()
